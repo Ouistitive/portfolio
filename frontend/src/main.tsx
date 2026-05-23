a421212e8d1db98b9592fac81d06b7255ba16b2b
Steven Tea <ouistitive@gmail.com>
2026-05-23 10:28:39 +0200

    feat: move all frontend app to dedicated folder

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
-import "../i18n";
+import "../i18n.js";
 import "./styles/App.css";
 import { UseScrollTop } from "./hooks/useScrollTop.tsx";
 
